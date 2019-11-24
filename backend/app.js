6b83d13fd10f8b0d2bb8e1bff7e223d059f292bd
Patric Schirrmann <patric.schirrmann@pschirrmann.de>
2019-11-24 12:50:24 +0100

    Adds first API REST method

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fasdajdhfjs',
+      title: 'First server side post',
+      content: 'This is coming from the server.'
+    },
+    {
+      id: 'asdasdafdlgkj',
+      title: 'Second server side post',
+      content: 'This is also coming from the server.'
+    },
+  ]
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
